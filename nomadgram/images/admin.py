784ceffc6191144f7c56d7cdd202e207e4bff5b4
maeng2418 <maeng2418@github.com>
2019-03-30 22:03:58 +0900

    Customizing the Django Admin (EditLink&SearchBar&Filter)

--- nomadgram/images/admin.py
+++ nomadgram/images/admin.py
@@ -5,14 +5,28 @@ from . import models
 
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
+    
+    list_display_links = (
+        'location',
+    )
+
+    search_fields = (
+        'location',
+        'caption',
+    )
+
+    list_filter = (
+        'location',
+        'creator',
+    )
 
     list_display = (
-        'created_at',
-        'updated_at',
         'file',
         'location',
         'caption',
         'creator',
+        'created_at',
+        'updated_at',
     )
 
 @admin.register(models.Like)
