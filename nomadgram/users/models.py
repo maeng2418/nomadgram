53139f6d8ec5ae873d831eb3c3875aa3ecf780f2
maeng2418 <maeng2418@github.com>
2019-03-30 10:36:41 +0900

    Creating The User Model

--- nomadgram/users/models.py
+++ nomadgram/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,7 +8,18 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    GENDER_CHOICES = (
+        ("male", "Male"),
+        ("female", "Female"),
+        ("not-specified", "Not specified")
+    )
+    # First Name and Last Name do not cover name patterns
+    # around the globe.
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(null=True, max_length=140)
+    gender = models.CharField(null=True, max_length=80, choices = GENDER_CHOICES)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
