3093779a9599cd4dc2df095a3cab98d4eb8d5d11
maeng2418 <maeng2418@github.com>
2019-04-05 00:11:29 +0900

    Viewing all the Comments and Likes (urls)

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -6,8 +6,26 @@ class ListAllImages(APIView):
 
     # request from browser.
     def get(self, request, format=None):
-        all_images =models.Image.objects.all() # All elements from Model.
+        all_images = models.Image.objects.all() # All elements from Model.
         
         serializer = serializers.ImageSerializer(all_images, many=True) # serializer deals only one. So, Set many=True for getting all data.
         
         return Response(data=serializer.data) # return converted Response.
+
+class ListAllComments(APIView):
+
+    def get(self, request, format=None):
+        all_comments = models.Comment.objects.all()
+
+        serializer = serializers.CommentSerializer(all_comments, many=True)
+
+        return Response(data=serializer.data)
+
+class ListAllLikes(APIView):
+
+    def get(self, request, format=None):
+        all_likes = models.Like.objects.all()
+
+        serializer = serializers.LikeSerializer(all_likes, many=True)
+
+        return Response(data=serializer.data)
