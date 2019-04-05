759dd37938ff47c96e34e23f31f8685b52693671
maeng2418 <maeng2418@github.com>
2019-04-05 23:39:31 +0900

    Hidden Model Fields in Django (_set)

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -1,15 +1,7 @@
 from rest_framework import serializers
 from . import models
-class ImageSerializer(serializers.ModelSerializer):
-
-    # Meta class is extra information
-    class Meta:
-        model = models.Image
-        fields = '__all__'
 
 class LikeSerializer(serializers.ModelSerializer):
-
-    image = ImageSerializer()
 
     # Meta class is extra information
     class Meta:
@@ -17,10 +9,26 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CommentSerializer(serializers.ModelSerializer):
-
-    image = ImageSerializer()
     
     # Meta class is extra information
     class Meta:
         model = models.Comment
         fields = '__all__'
+
+class ImageSerializer(serializers.ModelSerializer):
+
+    # Bring inside Object instead of the ID
+    comment_set = CommentSerializer(many=True)
+    like_set = LikeSerializer(many=True)
+
+    # Meta class is extra information
+    class Meta:
+        model = models.Image
+        fields = (
+            'id',
+            'file',
+            'location',
+            'caption',
+            'comment_set', # Letter use lower name of model.
+            'like_set'
+        )
