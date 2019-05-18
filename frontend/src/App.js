19e1329c4cd5829e745f3360db0052a47af21046
maeng2418 <maeng2418@github.com>
2019-05-19 00:20:12 +0900

    React Fragment Syntax

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import {ReactComponent as Logo} from './logo.svg';
 import styles from './App.module.scss';
 
 function App() {
   return (
     <div className={styles.app}>
       <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
+        <Logo/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
