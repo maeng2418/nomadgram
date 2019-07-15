5a7ff9368803b1863682d4f0d33f404ba57e2278
maeng <maeng2418@github.com>
2019-07-15 22:08:55 +0900

    Redux Middlewares Thunk

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,11 +1,14 @@
 // 나의 스토어를 설정/구성 (여러 리듀서를 한번에 모아서 관리)
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import users from 'redux/modules/users';
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
 
 const reducer = combineReducers({
     users
 });
 
-let store = initialState => createStore(reducer);
+let store = initialState => createStore(reducer, applyMiddleware(...middlewares));  // ...middlewares -> thunk, dsle, dlwek  배열이 unpack됨.
 
 export default store();
