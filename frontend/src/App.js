96dff288b54f5601f0d61f11f545f0f062bb7651
maeng <maeng2418@github.com>
2019-07-15 21:29:29 +0900

    Setting Up the Redux Store with Multiple Reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './App.module.scss';
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles.appHeader}>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className={styles.App} />;
+  }
 }
 
 export default App;
---
+++ frontend/src/redux/configureStore.js
@@ -0,0 +1,11 @@
+// 나의 스토어를 설정/구성 (여러 리듀서를 한번에 모아서 관리)
+import { createStore, combineReducers } from 'redux';
+import users from './modules/users';
+
+const reducer = combineReducers({
+    users
+});
+
+let store = initialState => createStore(reducer);
+
+export default store()
