d16b6dcf41395c32ca91c35de0bc0115062fc1c0
maeng <maeng2418@github.com>
2019-07-27 23:30:11 +0900

    Moving the App Component and Adding Reset-CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
-import App from 'App';
+import App from 'components/App';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from "connected-react-router";
 import store, { history } from "redux/configureStore";
-//import 'ReactotronConfig';
 import I18n from 'redux-i18n';
 import { translations } from 'translations'
 
